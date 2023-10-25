e4546d79fe40cccdd9000a36bfa88c7da440cf4c
PlacaFromHell <40618535+PlacaFromHell@users.noreply.github.com>
2023-10-25 00:33:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ class productManager {
     static id = 0;
 
     addProduct(title, description, price, thumbnail, stock, code) {
-        if (this.products.find((producto) => producto.title === title)){
-            console.log("ERROR: producto repetido.");
+        if (this.products.find((producto) => producto.code === code) || Array.from(arguments).length !== 6){
+            console.log("ERROR: producto repetido o campos faltantes.");
         } else {
-            this.products.push({title, description, price, thumbnail, stock, code:productManager.id});
+            this.products.push({title, description, price, thumbnail, stock, code, id:productManager.id});
             productManager.id++;
             console.log("Producto creado satisfactoriamente.");
         } 
@@ -20,8 +20,9 @@ class productManager {
     }
 
     getProductsById(id) {
-        if (this.products.find((producto) => producto.code === id)){
-            let retorno = "El producto se encuentra en la base de datos.";
+        const paquete = this.products.find((producto) => producto.id === id)
+        if (paquete) {
+            let retorno = paquete;
             return retorno;
         } else {
             let retorno = "ID no encontrada: si el problema persiste contacte con el administrador.";
@@ -36,12 +37,12 @@ const productos = new productManager;
 
 console.log(productos.getProducts()); //Muestra array vacío
 
-productos.addProduct("producto prueba", "Este es un producto de prueba", 200, "Sin imagen", 25, "ABC123");      //Crea producto
-productos.addProduct("producto prueba", "Este es un producto de prueba", 200, "Sin imagen", 25, "ABC123");      //Repetido, mensaje de error
-productos.addProduct("producto prueba 2", "Este es un producto de prueba 2", 200, "Sin imagen", 25, "ABC123");  //Crea producto
+productos.addProduct("producto prueba 0", "Este es un producto de prueba 0", 200, "Sin imagen", 25, "ABC123");      //Crea producto
+productos.addProduct("producto prueba 1", "Este es un producto de prueba 1", 200, "Sin imagen", 25, "ABC123");      //Repetido
+productos.addProduct("producto prueba 2", "Este es un producto de prueba 2", 200, "Sin imagen", 25);                //Falta
 
 console.log(productos.getProducts()); //Muestra array lleno
 
-console.log(productos.getProductsById(1)); //Muestra ID (existe)
+console.log(productos.getProductsById(0)); //Muestra ID (existe)
 
 console.log(productos.getProductsById(7)); //Muestra ID (no existe)
